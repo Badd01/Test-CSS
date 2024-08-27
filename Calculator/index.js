6030dcfc99d2eeb89a42a370ceca9433bcec254c
Badd <176387469+Badd01@users.noreply.github.com>
2024-08-28 01:13:32 +0700

    Logic for calculate

--- Calculator/index.js
+++ Calculator/index.js
@@ -1,6 +1,11 @@
 const display = document.querySelector(".display");
 
-let number1, number2, operator;
+const isNumber = (expression) => {
+  return !["+", "-", "*", "/"].includes(expression);
+};
+const isOperator = (expression) => {
+  return ["+", "-", "*", "/"].includes(expression);
+};
 
 const add = (number1, number2) => {
   return number1 + number2;
@@ -19,15 +24,38 @@ const divide = (number1, number2) => {
 
 const operate = (number1, operator, number2) => {
   if (operator == "+") {
-    add(number1, number2);
+    return add(number1, number2);
   }
   if (operator == "-") {
-    subtract(number1, number2);
+    return subtract(number1, number2);
   }
   if (operator == "*") {
-    multiply(number1, number2);
+    return multiply(number1, number2);
   }
   if (operator == "/") {
-    divide(number1, number2);
+    return divide(number1, number2);
   }
 };
+
+const equal = document.querySelector(".btn-equal");
+equal.addEventListener("click", () => {
+  let numbers = components.filter(isNumber);
+  let operator = components.filter(isOperator);
+  let result = operate(numbers[0], operator, numbers[1]);
+  display.innerHTML = result;
+});
+
+const clear = document.querySelector(".btn-clear");
+clear.addEventListener("click", () => {
+  display.innerHTML = "";
+  components = [];
+});
+
+const buttons = document.querySelectorAll(".btn");
+let components = [];
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    display.append(e.target.value);
+    components.push(e.target.value);
+  });
+});
