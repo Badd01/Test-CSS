7615c0e9c249c2ad655cea7a3347afc7fbc7a017
Badd <176387469+Badd01@users.noreply.github.com>
2024-08-31 00:14:51 +0700

    Final version

--- Calculator/index.js
+++ Calculator/index.js
@@ -17,6 +17,9 @@ const divide = (number1, number2) => {
 };
 
 const operate = (number1, calculate, number2) => {
+  // String to number
+  number1 = +number1;
+  number2 = +number2;
   switch (calculate) {
     case "+":
       return add(number1, number2);
@@ -31,33 +34,128 @@ const operate = (number1, calculate, number2) => {
   }
 };
 
-let firstNumber, theOperator, secondNumber;
-
 const content = document.querySelector(".content");
 const operator = document.querySelector(".operator");
-const expression = document.querySelector(".expression");
+// number character
+const maxLength = 44;
 const displayContent = (value) => {
-  return (content.textContent = value);
+  content.textContent = value;
+  if (content.textContent.length > maxLength) {
+    content.textContent = content.textContent.substring(0, maxLength);
+    return content.textContent;
+  }
+  return content.textContent;
 };
 const displayOperator = (value) => {
   return (operator.textContent = value);
 };
-const displayExpression = (value) => {
-  return (expression.textContent += value);
-};
 
-const buttonOperator = document.querySelectorAll(".btn-operator");
-buttonOperator.forEach((button) => {
-  button.addEventListener("click", (e) => {
-    displayOperator(e.target.value);
-    displayExpression(e.target.value);
-  });
-});
-
+let firstNumber, theOperator, secondNumber;
 const buttonNumber = document.querySelectorAll(".btn-number");
 buttonNumber.forEach((button) => {
   button.addEventListener("click", (e) => {
-    displayContent(e.target.value);
-    displayExpression(e.target.value);
+    if (secondNumber === undefined && theOperator === undefined) {
+      if (firstNumber === undefined) {
+        firstNumber = e.target.value;
+      } else {
+        firstNumber += e.target.value;
+        if (firstNumber > maxLength) {
+          firstNumber = firstNumber.substring(0, maxLength);
+        }
+      }
+      displayContent(firstNumber);
+    }
+
+    if (theOperator !== undefined) {
+      if (secondNumber === undefined) {
+        secondNumber = e.target.value;
+      } else {
+        if (secondNumber > maxLength) {
+          secondNumber = secondNumber.substring(0, maxLength);
+        }
+        secondNumber += e.target.value;
+      }
+      displayContent(secondNumber);
+    }
   });
+});
+
+let isEqualPressed = false; // State "="
+const buttonOperator = document.querySelectorAll(".btn-operator");
+buttonOperator.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    if (e.target.value !== "=") {
+      if (firstNumber !== undefined && secondNumber !== undefined) {
+        if (isEqualPressed === false) {
+          firstNumber = operate(firstNumber, theOperator, secondNumber);
+        }
+        displayContent(firstNumber);
+        secondNumber = undefined;
+      }
+      theOperator = e.target.value;
+      displayOperator(theOperator);
+      isEqualPressed = false;
+    } else {
+      if (firstNumber !== undefined && secondNumber !== undefined) {
+        firstNumber = operate(firstNumber, theOperator, secondNumber);
+        displayContent(firstNumber);
+        isEqualPressed = true;
+      }
+    }
+  });
+});
+
+const clear = () => {
+  content.textContent = "";
+  operator.textContent = "";
+  firstNumber = undefined;
+  secondNumber = undefined;
+  theOperator = undefined;
+};
+
+const backspace = () => {
+  if (secondNumber !== undefined) {
+    secondNumber = secondNumber.slice(0, -1);
+    displayContent(secondNumber);
+  } else if (firstNumber !== undefined) {
+    firstNumber = firstNumber.slice(0, -1);
+    displayContent(firstNumber);
+  }
+};
+
+//Clear and Delete
+const buttonFunction = document.querySelectorAll(".btn");
+buttonFunction.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    if (e.target.value === "C") {
+      clear();
+    }
+    if (e.target.value === "Del") {
+      backspace();
+    }
+  });
+});
+
+//Keyboard support
+window.addEventListener("keydown", (e) => {
+  const key = e.key;
+  if (key >= 0 && key <= 9) {
+    document.querySelector(`input[value="${key}"]`).click();
+  }
+  if (key === ".") {
+    document.querySelector('input[value="."]').click();
+  }
+  if (key === "+" || key === "-" || key === "*" || key === "/") {
+    document.querySelector(`input[value="${key}"]`).click();
+  }
+  if (key === "Enter" || key === "=") {
+    document.querySelector('input[value="="]').click();
+  }
+  if (key === "Backspace") {
+    document.querySelector('input[value="Del"]').click();
+  }
+  36;
+  if (key.toLowerCase() === "c") {
+    document.querySelector('input[value="C"]').click();
+  }
 });
